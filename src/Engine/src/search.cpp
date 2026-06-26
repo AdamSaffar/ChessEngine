23effd53ea53ce320ce55664e5d46bb8389a8a32
AdamSaffar <saffaradam4@gmail.com>
2026-06-26 17:26:11 -0400

    feat: implement alpha-beta pruning
    - Upgraged negamax to include alpha and beta bounds.

--- src/Engine/src/search.cpp
+++ src/Engine/src/search.cpp
@@ -12,7 +12,7 @@
  * Explores the game tree using Depth-First search,
  * Negamax assumes every node wants to maximize its own score.
  */
-int negamax(Board& board, int depth) {
+int negamax(Board& board, int depth, int alpha, int beta) {
     // Base case: Reached the end depth
     if (depth == 0) {
         return evaluate(board);
@@ -36,12 +36,23 @@ int negamax(Board& board, int depth) {
         legalMoves++;
         // --- NEGAMAX LOGIC ---
 
-        int score = -negamax(board, depth - 1);
+        int score = -negamax(board, depth - 1, -beta, -alpha);
 
         unmakeMove(move, board);
 
         // Keep the highest score
         maxScore = std::max(maxScore, score);
+
+        // --- ALPHA BETA PRUNING ---
+
+        alpha = std::max(alpha, maxScore); // update minimum guranteed score
+
+        // the cutoff
+        if (alpha >= beta) {
+            // min gauranteed score is higher than max score
+            // opponent will avoid branch entirely
+            break;
+        }
     }
     // Engine has reached a board state where it is impossible to make a move
     // Game is over: Either computer is checkmated or Stalemated
@@ -68,14 +79,16 @@ void searchRoot(Board& board, int depth) {
     int maxScore = -INF;
     int bestRootMove = 0;
 
+    int alpha = -INF;
+    int beta = INF;
     MoveList moveList;
     generateMoves(moveList, board);
 
-    for (int i = 0; moveList.count; i++) {
+    for (int i = 0; i < moveList.count; i++) {
         int move = moveList.moves[i];
         if (!makeMove(move, board)) continue;
-
-        int score = -negamax(board, depth - 1);
+        // pass inverted bounds
+        int score = -negamax(board, depth - 1, -beta, -alpha);
 
         unmakeMove(move, board);
 
@@ -83,6 +96,9 @@ void searchRoot(Board& board, int depth) {
             maxScore = score;
             bestRootMove = move; // Save the phyiscal move that caused the highest score
         }
+
+        // update minimum acceptable score(alpha)
+        alpha = std::max(alpha, maxScore);
     }
     bestMoveToPlay = bestRootMove;
 }
